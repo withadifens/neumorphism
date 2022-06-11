059e1d28cd23906eba245f4e8a8fc54674c6df22
Afiv Dicky Efendy <dickyareda@gmail.com>
2022-06-11 21:29:17 +0700

    reduce duration

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class _SplashScreenState extends State<SplashScreen> {
     // TODO: implement initState
     super.initState();
     Future.delayed(
-      const Duration(seconds: 5),
+      const Duration(seconds: 3),
       () => Navigator.of(context).pushReplacement(
         MaterialPageRoute(
           builder: (context) => const HomePage(),
